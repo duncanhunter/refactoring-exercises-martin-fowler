7b5db9b9d472275140b9a1f7ed4531d9c2a9c874
duncanhunter <duncan@duncanhunter.com.au>
2019-06-24 11:07:38 +1000

    - initial commit

---
+++ wallaby.js
@@ -0,0 +1,23 @@
+module.exports = function (wallaby) {
+    return {
+      files: [
+        'app/**/*.js',
+        '!app/**/*.spec.js'
+      ],
+      tests: [
+        'app/**/*.spec.js'
+      ],
+      env: {
+        type: 'node',
+        runner: 'node'
+      },
+      compilers: {
+        '**/*.js': wallaby.compilers.babel({
+          // Tell Wallaby to use Ava's Babel preset, necessary if your project doesn't use Babel otherwise.
+          presets: ['@ava/babel-preset-stage-4']
+        })
+      },
+      testFramework: 'ava',
+      debug: true
+    };
+  };
